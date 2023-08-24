5bea05545177e8ccd353b0bd29b074af2c20bb85
khanzzirfan <khanzz_irfan@hotmail.com>
2023-08-24 14:50:21 +1000

    chore: updated npm audit json

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,149 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { exec } from 'child_process';
+import { execSync, spawn } from 'child_process';
+import { promisify } from 'util';
 
-function updatePackageWithResolutions(auditFilePath, packageFilePath) {
+interface FixAvailable {
+  version: string;
+  isSemVerMajor: boolean;
+}
+
+interface Advisory {
+  name: string;
+  severity: string;
+  fixAvailable: FixAvailable;
+}
+
+const options = {
+  stdio: 'inherit',
+  maxBuffer: 1024 * 1024, // 1MB
+  encoding: 'utf8',
+};
+
+const execAsync = promisify(execSync);
+
+async function generateAuditJson() {
+  try {
+    execSync('npm audit --json > audit.json', {
+      stdio: 'inherit',
+      encoding: 'utf8',
+      maxBuffer: 1024 * 1024 * 100,
+    });
+
+    // Check if the audit.json file exists
+    if (fs.existsSync('audit.json')) {
+      console.log('audit.json generated.');
+    } else {
+      console.error('Error: audit.json was not generated.');
+    }
+    console.log('audit.json generated.');
+    return;
+  } catch (error: any) {
+    // Add a delay of 3 seconds
+    setTimeout(() => {
+      // Check again if the audit.json file exists
+      if (fs.existsSync('audit.json')) {
+        /// console.log('audit.json generated.');
+      } else {
+        console.error('Error: audit.json was not generated.');
+      }
+    }, 3000); // Delay of 3000 milliseconds (3 seconds)
+  }
+  return;
+}
+
+async function updatePackageWithResolutions(args: string[]) {
+  const auditFilePath = path.join(process.cwd(), 'audit.json');
+  const packageFilePath = path.join(process.cwd(), 'package.json');
+
+  // clean up audit.json file
+  if (fs.existsSync('audit.json')) {
+    // delete file
+    fs.unlinkSync('audit.json');
+  }
+
+  await generateAuditJson();
+  // // Generate audit.json file if it doesn't exist
+  if (!fs.existsSync(auditFilePath)) {
+    return;
+  }
+
+  console.log('completed npm audit');
+  console.log('analyzing audit.json');
+
   // Read audit.json file
-  fs.readFile(auditFilePath, 'utf8', (err, auditData) => {
-    if (err) {
-      console.error('Error reading audit.json:', err);
-      return;
+  const auditData = fs.readFileSync(auditFilePath, 'utf8');
+  const auditJson = JSON.parse(auditData);
+
+  // Find vulnerabilities with patches
+  const patches: { [key: string]: string } = {};
+  for (const [vulnId, vulnData] of Object.entries(auditJson.vulnerabilities)) {
+    const { name, severity, fixAvailable } = vulnData as Advisory;
+    const { version, isSemVerMajor } = fixAvailable || {};
+
+    if (!isSemVerMajor && version) {
+      patches[name] = version;
     }
 
-    try {
-      const auditDataParsed = JSON.parse(auditData);
+    if (args.includes('--major') && version) {
+      patches[name] = version;
+    }
+  }
 
-      const resolutions = {};
-      // Assuming auditDataParsed contains vulnerability data with patches
-      // You need to extract the relevant information and populate 'resolutions'
+  // Modify package.json file
+  const packageData = fs.readFileSync(packageFilePath, 'utf8');
+  const packageJson = JSON.parse(packageData);
+  if (!packageJson.resolutions) {
+    packageJson.resolutions = {};
+  }
+  packageJson.resolutions = {
+    ...packageJson.resolutions,
+    ...patches,
+  };
 
-      // Update package.json with resolutions
-      fs.readFile(packageFilePath, 'utf8', (err, packageData) => {
-        if (err) {
-          console.error('Error reading package.json:', err);
-          return;
-        }
+  // Write back to package.json
+  fs.writeFileSync(
+    packageFilePath,
+    JSON.stringify(packageJson, null, 2),
+    'utf8'
+  );
 
-        try {
-          const packageDataParsed = JSON.parse(packageData);
-          packageDataParsed.resolutions = resolutions;
-
-          // Write back to package.json
-          fs.writeFile(
-            packageFilePath,
-            JSON.stringify(packageDataParsed, null, 2),
-            'utf8',
-            (err) => {
-              if (err) {
-                console.error('Error writing to package.json:', err);
-                return;
-              }
-
-              console.log('package.json updated with resolutions.');
-
-              // Run npx force-resolutions using child process
-              exec('npx force-resolutions', (error, stdout, stderr) => {
-                if (error) {
-                  console.error('Error running npx force-resolutions:', error);
-                  return;
-                }
-                console.log('npx force-resolutions completed.');
-                console.log(stdout);
-              });
-            }
-          );
-        } catch (err) {
-          console.error('Error parsing package.json:', err);
-        }
+  // Run npm-force-resolutions to install all the patched versions
+  // Run npm-force-resolutions if --force flag is provided
+  if (args.includes('--force-resolution')) {
+    execAsync('npx npm-force-resolutions')
+      // @ts-ignore
+      .then(({ stdout, stderr }: { stdout: any; stderr: any }) => {
+        console.log('completed npm-force-resolutions');
+        console.log(stdout);
+        console.log(stderr);
+      })
+      .catch((err: any) => {
+        console.error(err);
       });
-    } catch (err) {
-      console.error('Error parsing audit.json:', err);
-    }
-  });
+
+    console.log('completed npm-force-resolutions');
+  } // EOF exec npm-force-resolutions
+  /// }); // EOF exec npm audit --json
+  console.log('completed upgrade. Check package.json for resolutions.');
+
+  // clean up audit.json file
+  if (fs.existsSync('audit.json')) {
+    // delete file
+    fs.unlinkSync('audit.json');
+    process.exit(0);
+  }
+}
+
+// Get command-line arguments
+const args = process.argv.slice(2);
+console.log('args', args);
+if (args.length <= 0) {
+  console.error(
+    'Usage: npx npm-update-resolutions -- --force-resolution --major'
+  );
+  process.exit(1);
 }
 
 // Usage
-updatePackageWithResolutions('audit.json', 'package.json');
+updatePackageWithResolutions(args);
